64ac0d1e50f8f7c001551aa9c629f0589ed547fb
Artur Bristot <128424203+arturbristot@users.noreply.github.com>
2023-11-10 14:14:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,21 +9,17 @@ def formulario():
 
 @app.route('/processar-formulario', methods=['POST'])
 
-def download_file():
-    # Substitua 'caminho/para/seu/arquivo/arquivo.zip' pelo caminho do seu arquivo
-    path = './downloads/bristotlindo.mp3'
-    # Substitua 'arquivo_para_download.zip' pelo nome que você deseja que o arquivo tenha ao ser baixado
-    filename = 'musica.mp3'
-    return send_file(path, as_attachment=True, download_name=filename)
-
-
 def processar_formulario():
     url = request.form.get('url')
-    if url:  # Verifica se a URL não é None ou vazia
+    if url:
         youtube = YouTube(url)
+        title = youtube.title
         audio = youtube.streams.filter(only_audio=True).first()
-        audio.download('./downloads/', filename='bristotlindo.mp3')
-        download_file()
+        audio.download('./downloads/', filename=f'{title}.mp3')
+        path = f'./downloads/{title}.mp3'
+        filename = f'{title}.mp3'
+        return send_file(path, as_attachment=True, download_name=filename)
+    
     return render_template('index.html', url=url)
 
 if __name__ == '__main__':
