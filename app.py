89e22a2619611017a960939c33acf26db9983019
Artur Bristot <arturbristot123@gmail.com>
2024-05-24 18:26:56 -0300

    update options

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, send_file
 from pytube import YouTube
+import os
 
 app = Flask(__name__)
 
@@ -8,19 +9,28 @@ def formulario():
     return render_template('index.html')
 
 @app.route('/processar-formulario', methods=['POST'])
-
 def processar_formulario():
     url = request.form.get('url')
-    if url:
+    format = request.form.get('format')
+    
+    if url and format:
         youtube = YouTube(url)
         title = youtube.title
-        audio = youtube.streams.filter(only_audio=True).first()
-        audio.download('./downloads/', filename=f'{title}.mp3')
-        path = f'./downloads/{title}.mp3'
-        filename = f'{title}.mp3'
-        return send_file(path, as_attachment=True, download_name=filename)
+        path = f'./downloads/{title}.{format}'
+        
+        if format == 'mp3':
+            audio = youtube.streams.filter(only_audio=True).first()
+            audio.download(output_path='./downloads/', filename=f'{title}.mp3')
+        elif format == 'mp4':
+            video = youtube.streams.get_highest_resolution()
+            video.download(output_path='./downloads/', filename=f'{title}.mp4')
+        
+        if os.path.exists(path):
+            return send_file(path, as_attachment=True, download_name=f'{title}.{format}')
+        else:
+            return f"Erro ao baixar o arquivo em formato {format}."
     
-    return render_template('index.html', url=url)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
