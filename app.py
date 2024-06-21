aed4ec734880cc3649a7a52e3fed10121cc4ea20
Artur Bristot <arturbristot123@gmail.com>
2024-06-20 21:46:26 -0300

    update playlists

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, send_file
-from pytube import YouTube
+from pytube import YouTube, Playlist
 import os
 
 app = Flask(__name__)
@@ -14,22 +14,35 @@ def processar_formulario():
     format = request.form.get('format')
     
     if url and format:
-        youtube = YouTube(url)
-        title = youtube.title
-        path = f'./downloads/{title}.{format}'
-        
-        if format == 'mp3':
-            audio = youtube.streams.filter(only_audio=True).first()
-            audio.download(output_path='./downloads/', filename=f'{title}.mp3')
-        elif format == 'mp4':
-            video = youtube.streams.get_highest_resolution()
-            video.download(output_path='./downloads/', filename=f'{title}.mp4')
-        
-        if os.path.exists(path):
-            return send_file(path, as_attachment=True, download_name=f'{title}.{format}')
-        else:
-            return f"Erro ao baixar o arquivo em formato {format}."
-    
+        try:
+            if 'playlist' in format.lower():  # Verifica se é pedido de playlist
+                playlist = Playlist(url)
+                for video in playlist.videos:
+                    if format.lower() == 'playlist mp3':
+                        audio = video.streams.filter(only_audio=True).first()
+                        audio.download(output_path='./downloads/', filename=f'{video.title}.mp3')
+                    elif format.lower() == 'playlist mp4':
+                        video = video.streams.get_highest_resolution()
+                        video.download(output_path='./downloads/', filename=f'{video.title}.mp4')
+            else:
+                youtube = YouTube(url)
+                title = youtube.title
+                path = f'./downloads/{title}.{format}'
+
+                if format == 'mp3':
+                    audio = youtube.streams.filter(only_audio=True).first()
+                    audio.download(output_path='./downloads/', filename=f'{title}.mp3')
+                elif format == 'mp4':
+                    video = youtube.streams.get_highest_resolution()
+                    video.download(output_path='./downloads/', filename=f'{title}.mp4')
+                
+                if os.path.exists(path):
+                    return send_file(path, as_attachment=True, download_name=f'{title}.{format}')
+                else:
+                    return f"Erro ao baixar o arquivo em formato {format}."
+        except Exception as e:
+            return f"Ocorreu um erro: {e}"
+
     return render_template('index.html')
 
 if __name__ == '__main__':
